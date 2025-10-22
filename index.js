3a98e73f72b3ff42013dca95a5b358070b5c7ffa
Manjiro <85405446+Manjiro303@users.noreply.github.com>
2025-10-22 08:17:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,4 @@
-# Create index.js
-cat > index.js << 'EOF'
 import { registerRootComponent } from 'expo';
 import App from './src/App';
 
 registerRootComponent(App);
-EOF
-
-# Update package.json to use it
-# (Edit package.json to change "main" field)
-
-# Commit and push
-git add index.js
-git commit -m "Add app entry point"
-git push
